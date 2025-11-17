dd4f8ab590fca2458d26a52c0d6e95e21ddc8942
brentalabag-ai <brentalabag@gmail.com>
2025-11-17 21:03:42 +0800

    darkmode button

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,7 @@ require_once "api/dashboard-b.php"
 <title>Dashboard - Queuing</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="css/common.css">
 <link rel="stylesheet" href="css/dashboard.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
--- student_login.php
+++ student_login.php
@@ -7,15 +7,18 @@ require_once "api/student-login-b.php"
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Student Login</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<link rel="stylesheet" href="css/common.css">
-<link rel="stylesheet" href="css/index.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
 
 </head>
 <body>
 
-<button class="dark-toggle"><i class="bi bi-moon-stars"></i></button>
+    <button class="dark-toggle btn btn-outline-secondary position-fixed top-0 end-0 m-3">
+        <i class="bi bi-moon-stars"></i>
+    </button>
 
 <div class="login-container card fade-in">
     <h1><i class="bi bi-person-circle"></i> Student Login</h1>
